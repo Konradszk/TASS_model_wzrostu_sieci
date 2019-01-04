9207a9156d90c63b545ba75969ebda3d1ec17877
konradszk <email@example.com>
2019-01-04 20:22:28 +0100

    - selenium was added - interia comment expander - get comment data - make json form data

--- main/webscaper/CommentDownloader.py
+++ main/webscaper/CommentDownloader.py
@@ -1,14 +1,55 @@
-import requests
+import json
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class CommentDownloader:
     def __init__(self, url):
         self.__url = url
         self.page = self.__get_page()
+        self.soup = None
 
     def __get_page(self):
-        file = requests.get(self.__url)
-        if file.status_code != 200:
-            print('Cannot get page: ', self.__url)
-            return
-        return file.content
+        self.__prepare_web_page()
+        return ''
+
+    def __prepare_web_page(self):
+        browser = webdriver.Chrome()
+        browser.get(self.__url)
+        browser.implicitly_wait(10)
+        browser.find_element_by_class_name('rodo-popup-agree').click()
+        WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, 'comments4-1'))
+        )
+        self.__expand_all_comment(browser)
+        element = browser.find_element_by_id('comments4-1')
+        self.get_comments(element.get_attribute('innerHTML'))
+        time.sleep(10)
+        #dodać przewijanie stron
+        browser.quit()
+
+    def __expand_all_comment(self, browser):
+        show_attr = browser.find_elements_by_class_name('forum__comment-replies-show')
+        for attr in show_attr:
+            attr.click()
+
+    def get_comments(self, html):
+        self.soup = BeautifulSoup(html, 'html.parser')
+        comment_div = self.soup.find_all(class_='forum__comment')
+        comments = {}
+        for comment in comment_div:
+            data = {'name': comment.find(class_='forum__comment-author-name').text,
+                    'date': comment.find(class_='forum__comment-author-date').text,
+                    'voteup': comment.find(class_='forum__comment-action--voteup').text,
+                    'votedown': comment.find(class_='forum__comment-action--votedown').text}
+            comments[data['name']] = data
+
+        json_comments = json.dumps(comments)
+        print(json_comments)
+        print(len(comment_div))
+        #trzeba dodać mechanizm łapania zagnieżdzeń
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from main import main
 print('Autorzy: Konrad Szczepaniak, Hubert Krawczyk')
 print('Skrypt wykonany na przedmot TASS 18Z')
 
-url = 'https://www.wykop.pl/link/4729775/cofanie-licznikow-mialo-byc-przestepstwem-wciaz-nie-jest/'
+url = 'https://fakty.interia.pl/malopolskie/news-tatry-ze-schroniska-ewakuowano-turystow,nId,2766003#comments4-1'
 
 print('Program został uruchomiony')
 main(url)
